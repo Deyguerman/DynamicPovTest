f988db6884acdcb5984372804e0740d541e09d5b
Deyguerman Guzman <deyguerman@gmail.com>
2020-09-17 17:32:39 -0400

    Testing deepstream

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@
             transition-hide="scale"
             label="Destinations"
             v-model="portsCodes"
+            @input="handlerChange($event)"
             :options="destinations"
             :option-value="
               (opt) =>
@@ -45,7 +46,7 @@
 <script>
 import Places from "./components/Places.vue";
 import axios from "axios";
-
+const { DeepstreamClient } = window.DeepstreamClient;
 export default {
   name: "LayoutDefault",
   components: {
@@ -53,16 +54,46 @@ export default {
   },
   data() {
     return {
+      ds: null,
+      scroll: null,
+      places: {},
       portsCodes: null,
       destinations: [],
     };
   },
   created() {
+    this.ds = new DeepstreamClient("192.168.0.10:6020"); // Replace with the Ip Server
+    this.ds.login();
+    this.places = this.ds.record.getRecord("test/place");
+
+    this.ds.event.subscribe("setPortsCodes", (eventData) => {
+      this.portsCodes = eventData;
+    });
+
+    this.ds.event.subscribe("scrollPage", (eventData) => {
+      window.scrollTo(eventData.x, eventData.y);
+    });
+
     axios.get("./data/ports.json").then((response) => {
       this.getDestinationList(response.data, "destination");
     });
   },
+  mounted() {
+    window.addEventListener("scroll", () => {
+      clearTimeout(this.scroll);
+      this.scroll = setTimeout(() => {
+        this.ds.event.emit("scrollPage", {
+          x: window.scrollX,
+          y: window.scrollY,
+        });
+      }, 500);
+    });
+  },
   methods: {
+    handlerChange(value) {
+      this.places.set(value);
+      this.ds.event.emit("setPortsCodes", value);
+    },
     getDestinationList(list, key) {
       // Get Array uniques port names
       const destinationNames = [...new Set(list.map((item) => item[key]))];
